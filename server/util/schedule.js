5264083931386bd3826cb71705699d644177fef0
bemazepedaib <benjaminzepedaibarra@gmail.com>
2023-06-14 09:20:09 -0700

    changed fetch import

--- server/util/schedule.js
+++ server/util/schedule.js
@@ -1,7 +1,7 @@
 const { ApolloClient, gql, InMemoryCache, HttpLink } = require('apollo-boost');
 const schedule = require('node-schedule');
-const fetch = require('node-fetch')
-require('dotenv').config({ path: '../../.env' })
+import fetch from 'node-fetch';
+require('dotenv').config({ path: '../../.env' });
 
 const link = new HttpLink({
     uri: 'https://naayari-tours-backend.up.railway.app/NaayarAPI',
